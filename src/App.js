86797c0e037ee9ce1bcc9e00a0863c136ccc47a4
Sun5hine <af5kh@yahoo.com>
2019-01-19 21:42:26 +0000

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Form from "./Form.js"
+import Me from "./Me.js"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Me />
+      <Form />
+      <h1> let's get cracking </h1>
+
+
       </div>
     );
   }
