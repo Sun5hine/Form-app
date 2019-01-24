7aae4d5d456cddf198fc4a1bfeba6a52fb07f476
Sun5hine <af5kh@yahoo.com>
2019-01-24 21:55:36 +0000

    some changes

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Me />
-      <Form />
+
       <Timer />
 
 
--- src/Timer.js
+++ src/Timer.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
 
-class Me extends React.Component {
-  render() {
-    return (
-      <div>
-  <h1> Hey You </h1>
-  <h2> Testing the git </h2>
-      </div>
-    );
-  }
+class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {seconds: 0};
+    }
+
+    tick() {
+      this.setState(state => ({
+        seconds: state.seconds + 1
+      }));
+    }
+     ComponentDidMount(){
+       this.interval = setInterval(()=> this.tick(),1000);
+     }
+     ComponentWillUMount(){
+       clearInterval(this.interval);
+     }
+    render(){
+      return(
+        <div>
+          Seconds: {this.state.seconds}
+        </div>
+      );
+    }
 }
 
-export default Me;
+export default Timer;
