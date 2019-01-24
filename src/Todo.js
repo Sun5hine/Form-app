4f9d3b2b2914134d93ae96175d10c8b9f14ddf8e
Sun5hine <af5kh@yahoo.com>
2019-01-24 23:26:21 +0000

    some changes

--- src/Todo.js
+++ src/Todo.js
@@ -14,9 +14,45 @@ class Todo extends React.Component {
       <form onSubmit = {this.handleSubmit}>
       <label htmlFor="new-todo">
       What you want to do?
+      </label>
+      <input id="new-todo"
+      onChange = {this.handleChange}
+      value = {this.state.text} />
+      <button> Add #{this.state.items.length + 1 }</button>
+      </form>
       </div>
     );
   }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (!this.state.text.length) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now()
+    };
+    this.setState(state => ({
+      items: state.items.concat(newItem),
+      text: ''
+    }));
+  }
 }
 
+  class TodoList extends React.Component {
+    render(){
+      return(
+        <ul>
+        {this.props.items.map(item => (
+          <li> key={item.id>}
+        ))}
+      )
+    }
+
+  }
 export default Todo;
