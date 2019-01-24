e1f1ff3596fad1171a206f7d4fe83bec51d76e32
Sun5hine <af5kh@yahoo.com>
2019-01-24 23:15:20 +0000

    some changes

--- src/Todo.js
+++ src/Todo.js
@@ -4,13 +4,14 @@ import React from 'react';
 class Todo extends React.Component {
   constructor(props) {
     super(props);
-    
+    this.state = {items: [], text: ''};
   }
   render(){
     return(
-
       <div>
-      Testing
+      <h3> TODO </h3>
+      <TodoList items = {this.state.items} />
+      <form onSubmit = {this.handleSubmit}>
       </div>
     );
   }
